1084ebb9d6eef86ce787aff4a3195f013091fcf7
huyjaky <huyphanuyg@gmail.com>
2024-07-18 23:46:56 +0700

    astro-nvim-v2 - add more themes for alpha home screen

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,19 +15,20 @@ return {
     auto_install = true,
 
     -- List of parsers to ignore installing (or "all")
-    ignore_install = { "javascript", "typescript", "tsx" },
+    ignore_install = { "javascript", "typescript", "tsx" , "python"},
     
     ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
     -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
     highlight = {
-      enable = true,
+      enable = false,
 
       -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
       -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
       -- the name of the parser)
       -- list of language that will be disabled
-      disable = { "c", "rust", "typescript", "tsx" },
+      -- disable = { "c", "rust", "typescript", "tsx" },
+      disable = { "c", "rust"},
       
       -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
       -- disable = function(lang, buf)
@@ -40,7 +41,7 @@ return {
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+      -- additional_vim_regex_highlighting = false,
     },
   },
 }
